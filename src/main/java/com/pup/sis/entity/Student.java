fe8470f9acdf9657ddaea14abaf7de3433d35d19
Sylveighty <kaklikesmilfs3@gmail.com>
2026-06-05 21:35:15 +0800

    feat(entity): add Student entity

---
+++ src/main/java/com/pup/sis/entity/Student.java
@@ -0,0 +1,29 @@
+package com.pup.sis.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String studentNumber;
+
+    private Integer yearLevel;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
